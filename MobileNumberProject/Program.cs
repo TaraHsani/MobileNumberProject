c5b86dcb70694800f65c5e73cd2bfe8f5d4bb6ee
TaraHsani <147997426+TaraHsani@users.noreply.github.com>
2023-10-28 15:48:33 +0200

    Update!

--- MobileNumberProject/Program.cs
+++ MobileNumberProject/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("What is your mobilenumber?");
             string mobilenumber = Console.ReadLine();
             string mobilnumber = mobilenumber.Trim();
-            string newSub = mobilenumber.Substring(0 , 3);
+            string newSub = mobilenumber.Substring(0 , 5);
             Console.WriteLine(newSub);
             
 
