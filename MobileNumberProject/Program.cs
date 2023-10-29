b3cdc2605695074b90dbbacf5360a1cd234ba625
TaraHsani <147997426+TaraHsani@users.noreply.github.com>
2023-10-29 12:53:12 +0100

    Update with branchingCsharp.

--- MobileNumberProject/Program.cs
+++ MobileNumberProject/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MobileNumberProject
+﻿using System.Runtime;
+
+namespace MobileNumberProject
 {
     internal class Program
     {
@@ -6,10 +8,29 @@
         {
             Console.WriteLine("What is your mobilenumber?");
             string mobilenumber = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(mobilenumber))
+            {
+                Console.WriteLine("Please enter your mobilnumber!");
+
+                return;
+
+            }
             string mobilnumber = mobilenumber.Trim();
-            string newSub = mobilenumber.Substring(0 , 5);
+            string newSub = mobilenumber.Substring(0, 5);
             Console.WriteLine(newSub);
-            
+            string gender = Console.ReadLine(); //F or M
+            string username = Console.ReadLine();
+            int resualt = 0;
+
+
+            if (newSub == "+98" || gender == "F" || username == "tara" || resualt == 0)
+            {
+
+                Console.WriteLine();
+
+            }
+
 
         }
     }
